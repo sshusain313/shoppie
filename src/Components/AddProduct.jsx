00f69a4c3e5e68ea34d606a73c5fc10f5c971674
shabahat <shabahatsyed101@gmail.com>
2025-01-12 21:20:29 +0530

    fourth commit

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -31,7 +31,7 @@ const AddProduct = () => {
     let formData=new FormData();
     formData.append('product', image);
 
-    await fetch('http://localhost:4000/upload',{
+    await fetch('https://mernshop-8c3g.onrender.com/upload',{
       method: 'POST',
       headers:{
         Accept: 'application/json',
@@ -42,7 +42,7 @@ const AddProduct = () => {
     if(responseData.success){
       product.image=responseData.image_url;
       console.log(product);
-      await fetch('http://localhost:4000/addproduct', {
+      await fetch('https://mernshop-8c3g.onrender.com/addproduct', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
